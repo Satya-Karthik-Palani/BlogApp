a47b81e265ed3eedc3f0111cc38c3fb168e38819
Satya-Karthik-Palani <karthik.palani0827@gmail.com>
2024-02-23 20:52:48 +0530

    Changed the preflight settings for cors

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const comment = require("./routes/commentRoute");
 
 // https://blogapp-frontend-pa49.onrender.com
 // https://localhost:3000
-app.use(cors({origin:"https://blogapp-frontend-pa49.onrender.com",credentials: true,methods: ["GET", "POST", "PUT", "DELETE"],allowedHeaders: ["Content-Type", "Authorization"] }));
+app.use(cors({origin:"https://blogapp-frontend-pa49.onrender.com",credentials: true,methods: ["GET", "POST", "PUT", "DELETE"],allowedHeaders: ["Content-Type", "Authorization"],optionsSuccessStatus: 200  }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
